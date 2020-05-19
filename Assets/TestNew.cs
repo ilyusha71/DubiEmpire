1d3b21f4bf4803f737209215dc4dee9ec345aacb
unknown <ilyusha71@gmail.com>
2020-05-19 08:07:48 +0800

    0519

--- Assets/TestNew.cs
+++ Assets/TestNew.cs
@@ -15,20 +15,20 @@ public class TestNew : MonoBehaviour
 
     public Text ttt;
 
-    void Awake ()
+    void Awake()
     {
-        crafts = seri.GetComponentsInChildren<Kocmoca.Protodesign> ();
+        crafts = seri.GetComponentsInChildren<Kocmoca.Protodesign>();
         cmFreeLook = new CinemachineFreeLook[crafts.Length];
         for (int i = 0; i < crafts.Length; i++)
         {
-            if (crafts[i].GetComponentInChildren<Animator> ())
-                crafts[i].GetComponentInChildren<Animator> ().enabled = false;
-            cmFreeLook[i] = crafts[i].GetComponentInChildren<CinemachineFreeLook> ();
+            if (crafts[i].GetComponentInChildren<Animator>())
+                crafts[i].GetComponentInChildren<Animator>().enabled = false;
+            cmFreeLook[i] = crafts[i].GetComponentInChildren<CinemachineFreeLook>();
         }
         // cmsFreeLook.enabled = true;
     }
 
-    void Center ()
+    void Center()
     {
         for (int i = 0; i < crafts.Length; i++)
         {
@@ -43,46 +43,52 @@ public class TestNew : MonoBehaviour
                 cmFreeLook[i].m_Heading.m_Bias = 135.0f;
             }
         }
-        crafts[index].GetComponent<Kocmoca.Protodesign> ().AlignCentre ();
+        crafts[index].GetComponent<Kocmoca.Protodesign>().AlignCentre();
         // GetComponent<Camera> ().orthographicSize = 7.0f / crafts[index].GetComponent<Kocmoca.Protodesign> ().GetScalePower ();
     }
 
-    void DDD ()
+    void DDD()
     {
-        RenderTexture rt = GetComponent<Camera> ().targetTexture;
+        RenderTexture rt = GetComponent<Camera>().targetTexture;
         //    = Selection.activeObject as RenderTexture;
 
         RenderTexture.active = rt;
-        Texture2D tex = new Texture2D (rt.width, rt.height, TextureFormat.RGB24, false);
-        tex.ReadPixels (new Rect (0, 0, rt.width, rt.height), 0, 0);
-        tex.Apply ();
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        tex.Apply();
         RenderTexture.active = null;
 
         byte[] bytes;
-        bytes = tex.EncodeToPNG ();
+        bytes = tex.EncodeToPNG();
 
         string path = "Assets/" + crafts[index].transform.name + ".png";
-        System.IO.File.WriteAllBytes (path, bytes);
+        System.IO.File.WriteAllBytes(path, bytes);
         // AssetDatabase.ImportAsset(path);
-        Debug.Log ("Saved to " + path);
+        Debug.Log("Saved to " + path);
     }
     // public CinemachineFreeLook cmFreeLook;
 
     // Update is called once per frame
-    void Update ()
-    {
-        if (Input.GetKeyDown (KeyCode.P))
-            DDD ();
 
-        if (Input.GetKeyDown (KeyCode.D))
+public string GetCraftName()
+{
+    return  crafts[index].transform.name;
+}
+
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.P))
+        //     DDD();
+
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            index = (int) Mathf.Repeat (++index, crafts.Length);
-            Center ();
+            index = (int)Mathf.Repeat(++index, crafts.Length);
+            Center();
         }
-        if (Input.GetKeyDown (KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            index = (int) Mathf.Repeat (--index, crafts.Length);
-            Center ();
+            index = (int)Mathf.Repeat(--index, crafts.Length);
+            Center();
         }
         // if (Input.GetKey (KeyCode.Mouse1))
         // {
@@ -104,23 +110,34 @@ public class TestNew : MonoBehaviour
         //     cmFreeLook[index].m_XAxis.m_InputAxisValue = 0;
         //     cmFreeLook[index].m_YAxis.m_InputAxisValue = 0;
         // }
-        if (Input.GetAxis ("Mouse ScrollWheel") != 0)
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            oSize = Mathf.Clamp (oSize -= Input.GetAxis ("Mouse ScrollWheel") * 3, 0.1f, 15.0f);
+            oSize = Mathf.Clamp(oSize -= Input.GetAxis("Mouse ScrollWheel") * 1.5f, 0.1f, 15.0f);
             // cmFreeLook[index].m_Lens.OrthographicSize -= Input.GetAxis ("Mouse ScrollWheel") * 37;
             // radius = Mathf.Clamp (radius -= Input.GetAxis ("Mouse ScrollWheel") * 37, 2.7f, 18.2f);
         }
-        cmFreeLook[index].m_Lens.OrthographicSize = Mathf.Lerp (cmFreeLook[index].m_Lens.OrthographicSize, oSize, 0.073f);
+        // cmFreeLook[index].m_Lens.OrthographicSize = Mathf.Lerp (cmFreeLook[index].m_Lens.OrthographicSize, oSize, 0.073f);
+        GetComponent<Camera>().orthographicSize = oSize;
         // for (int i = 0; i < 2; i++)
         // {
         //     cmFreeLook[index].m_Orbits[i].m_Radius = Mathf.Lerp (cmFreeLook[index].m_Orbits[i].m_Radius, radius, 0.073f);
         // }
+        if (Input.GetKey(KeyCode.D))
+            transform.localPosition -= Vector3.right * 0.001f;
+        if (Input.GetKey(KeyCode.A))
+            transform.localPosition += Vector3.right * 0.001f;
+            if (Input.GetKey(KeyCode.W))
+            transform.localPosition -= Vector3.up * 0.001f; 
+            if (Input.GetKey(KeyCode.S))
+            transform.localPosition += Vector3.up * 0.001f;
+        if (Input.GetKey(KeyCode.X))
+            transform.localPosition = Vector3.zero;
 
         Vector3 one = crafts[index].transform.position - transform.position;
-        Vector3 two = new Vector3 (one.x, 0, one.z);
+        Vector3 two = new Vector3(one.x, 0, one.z);
 
-        float angle = Vector3.Angle (one, two); //求出两向量之间的夹角
-        ttt.text = angle.ToString ();
+        float angle = Vector3.Angle(one, two); //求出两向量之间的夹角
+        ttt.text = angle.ToString();
     }
     float radius, oSize;
 }
